941d22bef9aa807b4a7f89f4e4996d7394066348
Nu Appleblossom <nu_appleblossom@MacBooks-MBP-2.broadband>
2023-10-02 07:54:34 +0100

    added 'posN' criteria_mode, filtering for tokens with a target letter in position N

--- src/get_training_data.py
+++ src/get_training_data.py
@@ -1,37 +1,48 @@
 # This builds a dataset for training a linear probe to classify tokens according to various criteria
-# criterion: should be "anywhere", "starting", "length" or "distinct"
+# criteria_mode: should be "anywhere", "starting", "posN" (where N is a digit), "length" or "distinct"
 # target: should be an all-Roman string if criterion == "anywhere" or "starting"
 #  or else should be a positive integer in string form if criterion = "length" or "distinct"
 
+import re
+
 def get_training_data(
-		criterion, 
-		target, 
+		criteria_mode, 
+		target,    # this will be a short string for "anywhere", "starting" or "posN"; should be a numerical string for "length" or "distinct"
 		num_samples, 
 		embeddings, 
 		all_rom_token_gt2_indices,
 		token_strings):
 
 	# Fetch indices for tokens that match the required pattern
-	if criterion == "anywhere": #required pattern is target string appears anywhere as substring of token
+	if criteria_mode == "anywhere": #required pattern is target string appears anywhere as substring of token
 		if not target.isalpha():
 			return None
 		else:		
 			positive_indices = [index for index in all_rom_token_gt2_indices if target.lower() in token_strings[index].lstrip().lower()]
 
-	elif criterion == "starting": #required pattern is target string appears as initial substring of lstrip'd token
+	elif criteria_mode == "starting": #required pattern is target string appears as initial substring of lstrip'd token
 		if not target.isalpha():
 			return None
 		else:		
 			positive_indices = [index for index in all_rom_token_gt2_indices if token_strings[index].lstrip().lower().startswith(target.lower())]
 
+	elif bool(re.match(r'^pos[0-9]$', criteria_mode)):   # required pattern is target string (must be single character) is in position N whre criteria_mode == 'posN'
+		if not (target.isalpha() and len(target) == 1):
+			return None
+		else:		
+			positive_indices = []
+			for index in all_rom_token_gt2_indices:
+				token_string = token_strings[index].lstrip().lower()
+				if len(token_string) > int(criteria_mode[-1]) and token_string[int(criteria_mode[-1])] == target:
+					positive_indices.append(index)
 
-	elif criterion == "length": #required pattern is token is of length int(target)
+	elif criteria_mode == "length": #required pattern is token is of length int(target)
 		if not target.isdigit() or (target.isdigit() and int(target) == 0):
 			return None
 		else:
 			positive_indices = [index for index in all_rom_token_gt2_indices if len(token_strings[index].lstrip()) == int(target)]
 
-	elif criterion == "distinct": #required pattern is token has int(target) distinct characters
+	elif criteria_mode == "distinct": #required pattern is token has int(target) distinct characters
 		if not target.isdigit() or (target.isdigit() and int(target) == 0):
 			return None
 		else:
