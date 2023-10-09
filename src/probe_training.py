818fe74ee86da2d4ac580559b086d825b173129f
mwatkins1970 <drsyntax@hotmail.com>
2023-10-09 16:50:14 +0000

    Minor refactoring of probe_training.

--- src/probe_training.py
+++ src/probe_training.py
@@ -32,6 +32,22 @@ except ImportError:
     from get_training_data import get_training_data
 
 
+
+#RANDOM SEED STUFF HERE (PROBABLY MISGUIDED!)
+rnd_seed = 42
+
+torch.manual_seed(rnd_seed)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(rnd_seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+np.random.seed(rnd_seed)
+
+hidden_dim=128
+num_hidden_layers=2
+
+
 def create_and_log_artifact(tensor, name, artifact_type, description):
     # Use a temporary file to save the tensor
     with tempfile.NamedTemporaryFile(delete=True, suffix='.pt') as tmp:
@@ -139,6 +155,7 @@ def train_letter_probe_runner(
                     "model_name": "gpt-j",
                     "probe_type": probe_type,
                     "train_test_split": 0.2,
+                    "seed": rnd_seed,
                     "case_sensitive": False,
                     "batch_size": 32,
                     "learning_rate": 0.001,
@@ -162,13 +179,13 @@ def train_letter_probe_runner(
         criteria_mode, letter, num_samples, embeddings, all_rom_token_indices, token_strings)
         
         # split the data into training and validation sets (using a function from the sklearn.model_selection module)
-        X_train, X_val, y_train, y_val = train_test_split(all_embeddings, all_labels, test_size=0.2, random_state=42, stratify=all_labels)
+        X_train, X_val, y_train, y_val = train_test_split(all_embeddings, all_labels, test_size=0.2, random_state=rnd_seed, stratify=all_labels)
         
         # Initialize model and optimizer based on probe_type
         if probe_type == 'linear':
             model = LinearProbe(embeddings_dim).to(device)
         elif probe_type == 'mlp':
-            model = MLPProbe(embeddings_dim, hidden_dim=128, num_hidden_layers=2).to(device)
+            model = MLPProbe(embeddings_dim, hidden_dim=hidden_dim, num_hidden_layers=num_hidden_layers).to(device)
         
         optimizer = optim.Adam(model.parameters(), lr = 0.001)
         criterion = nn.BCEWithLogitsLoss()         # Binary cross-entropy loss with logits (because we haven't used an activation in our model)
@@ -183,10 +200,6 @@ def train_letter_probe_runner(
         #X_val, y_val (embeddings and labels for validation) were likewise created above using standard methods applied to all_embeddings and all_labels tensors
         
         # TRAINING LOOP
-        
-        # initialise relevant variables for early stopping
-        best_val_loss = float('inf')
-        no_improve_count = 0
         
         print('\n_________________________________________________\n')
         for epoch in range(num_epochs):
@@ -213,97 +226,90 @@ def train_letter_probe_runner(
                 
             print(f"{letter}: epoch {epoch + 1}/{num_epochs}, Loss: {total_loss / len(train_loader)}")
 
-            if use_wandb:
-                if probe_type == 'linear':
-                    artifact_name = f"probe_weights_for_{letter.upper()}_criterion_{criteria_mode}"
-                    print("Now logging probe weight tensor wandb artifact...")
-                    create_and_log_artifact(
-                        probe_weights_tensor,
-                        artifact_name,
-                        "model_tensors",
-                        f"Letter presence probe weight tensor for {letter.upper()} with criterion_{criteria_mode}"
-                    )
-                elif probe_type == 'mlp':  
-                    pass  # Don't log the weights for mlp
+
                 
+            # EVALUATION (VALIDATION) PHASE
 
-                # EVALUATION (VALIDATION) PHASE
+            # Set the model to evaluation mode
+            model.eval()
 
-                # Set the model to evaluation mode
-                model.eval()
+            # Create DataLoader for validation data
+            val_dataset = LetterDataset(X_val, y_val)
+            val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
 
-                # Create DataLoader for validation data
-                val_dataset = LetterDataset(X_val, y_val)
-                val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+            # Keep track of correct predictions and total predictions
+            correct_preds = 0
+            total_preds = 0
+            validation_loss = 0.0
 
-                # Keep track of correct predictions and total predictions
-                correct_preds = 0
-                total_preds = 0
-                validation_loss = 0.0
+            with torch.no_grad():  # Ensure no gradients are computed during validation
+                    all_labels = []  # Store all true labels
+                    all_predictions = []  # Store all model predictions
 
-                with torch.no_grad():  # Ensure no gradients are computed during validation
-                        all_labels = []  # Store all true labels
-                        all_predictions = []  # Store all model predictions
+                    for batch_embeddings, batch_labels in val_loader:
+                            batch_embeddings = batch_embeddings.to(device).float()  # Ensure embeddings are on the correct device and dtype
+                            batch_labels = batch_labels.to(device).float()  # Ensure labels are on the correct device and dtype
 
-                        for batch_embeddings, batch_labels in val_loader:
-                                batch_embeddings = batch_embeddings.to(device).float()  # Ensure embeddings are on the correct device and dtype
-                                batch_labels = batch_labels.to(device).float()  # Ensure labels are on the correct device and dtype
+                            outputs = model(batch_embeddings).squeeze()
 
-                                outputs = model(batch_embeddings).squeeze()
+                            # Calculate loss on validation data
+                            loss = criterion(outputs, batch_labels)
+                            validation_loss += loss.item()  # Update validation loss
 
-                                # Calculate loss on validation data
-                                loss = criterion(outputs, batch_labels)
-                                validation_loss += loss.item()  # Update validation loss
+                            # Convert outputs to probabilities
+                            probs = torch.sigmoid(outputs)
+                            predictions = (probs > 0.5).float()
 
-                                # Convert outputs to probabilities
-                                probs = torch.sigmoid(outputs)
-                                predictions = (probs > 0.5).float()
+                            # Update correct and total predictions
+                            correct_preds += (predictions == batch_labels).sum().item()
+                            total_preds += batch_labels.size(0)
 
-                                # Update correct and total predictions
-                                correct_preds += (predictions == batch_labels).sum().item()
-                                total_preds += batch_labels.size(0)
+                            # Append batch labels and predictions to all_labels and all_predictions
+                            all_labels.append(batch_labels.cpu().numpy())
+                            all_predictions.append(predictions.cpu().numpy())
 
-                                # Append batch labels and predictions to all_labels and all_predictions
-                                all_labels.append(batch_labels.cpu().numpy())
-                                all_predictions.append(predictions.cpu().numpy())
+                    # Flatten all_labels and all_predictions lists and convert to numpy arrays
+                    all_labels = np.concatenate(all_labels)
+                    all_predictions = np.concatenate(all_predictions)
 
-                        # Flatten all_labels and all_predictions lists and convert to numpy arrays
-                        all_labels = np.concatenate(all_labels)
-                        all_predictions = np.concatenate(all_predictions)
+                    # Compute F1 Score
+                    f1 = f1_score(all_labels, all_predictions)
 
-                        # Compute F1 Score
-                        f1 = f1_score(all_labels, all_predictions)
+                    validation_loss /= len(val_loader)  # Get the average validation loss
 
-                        validation_loss /= len(val_loader)  # Get the average validation loss
-
-                        if validation_loss < best_val_loss:
-                            best_val_loss = validation_loss
-    
+                    if probe_type == 'linear':
+                        probe_weights_tensor = model.weight.data.clone().detach()  
                         if use_wandb:
-                            if probe_type == 'linear':
-                                artifact_name = f"probe_weights_for_{letter.upper()}_criterion_{criteria_mode}"
-                                print("Now logging probe weight tensor wandb artifact...")
-                                create_and_log_artifact(
-                                    probe_weights_tensor,
-                                    artifact_name,
-                                    "model_tensors",
-                                    f"Letter presence probe weight tensor for {letter.upper()} with criterion_{criteria_mode}"
-                                )
-                        elif probe_type == 'mlp':  
-                            # Skip logging weights for mlp, since you're not storing or using them
-                            pass
+                            artifact_name = f"probe_weights_for_{letter.upper()}_criterion_{criteria_mode}"
+                            print("Now logging probe weight tensor wandb artifact...")
+                            create_and_log_artifact(
+                                probe_weights_tensor,
+                                artifact_name,
+                                "model_tensors",
+                                f"Letter presence probe weight tensor for {letter.upper()} with criterion_{criteria_mode}"
+                            )
+
+                      elif probe_type == 'mlp':  
+                          # Skip logging weights for mlp, since you're not storing or using them
+                          pass
 
 
-                        # Calculate accuracy and average loss
-                        accuracy = correct_preds / total_preds
-                        print(f"Validation Accuracy: {accuracy * 100:.2f}%")
-                        print(f"Validation Loss: {validation_loss:.4f}")
-                        print(f"F1 Score: {f1:.4f}\n")
+                    # Calculate accuracy and average loss
+                    accuracy = correct_preds / total_preds
+                    print(f"Validation Accuracy: {accuracy * 100:.2f}%")
+                    print(f"Validation Loss: {validation_loss:.4f}")
+                    print(f"F1 Score: {f1:.4f}\n")
 
-                        if use_wandb:
-                                wandb.log({"validation_loss": validation_loss})
-                                wandb.log({"validation_accuracy": accuracy})
-                                wandb.log({"f1_score": f1})
+                    if use_wandb:
+                        wandb.log({
+                            "validation_loss": validation_loss,
+                            "validation_accuracy": accuracy,
+                            "f1_score": f1
+                        })
+
+
+        if use_wandb:
+            wandb.finish()
 
         if probe_type == 'linear':
             return probe_weights_tensor
