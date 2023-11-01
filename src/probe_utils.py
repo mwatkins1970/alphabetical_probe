99195c70691c6d0bcdf95932887abcff8ffdaf6e
mwatkins1970 <108693675+mwatkins1970@users.noreply.github.com>
2023-11-01 16:45:38 +0000

    Update to probe_utils.py (some new utilities added)

--- src/probe_utils.py
+++ src/probe_utils.py
@@ -1,10 +1,9 @@
-# This separates all-Roman tokens into those whose first letter can/not be predicted successfully
-# by looking at closest "starting letter" linear probe 
+import torch
+import torch.nn.functional as F
+from collections import OrderedDict
 
-import torch 
-
-def find_closest_probe(embedding_vector, probes_tensor):    
-    # probes_tensor should be shape-(26,4096), with rows corresponding to alphabet
+def find_closest_probe(embedding_vector, probes_tensor):
+    # probes_tensor should be shape-(26, 4096), with rows corresponding to the alphabet
 
     # Check if all rows are zeros
     if torch.all(probes_tensor == 0):
@@ -16,19 +15,109 @@ def find_closest_probe(embedding_vector, probes_tensor):
     # Filter out zero rows from probes_tensor
     filtered_probes_tensor = probes_tensor[non_zero_mask]
     
+    # Ensure both tensors are on the same device
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    embedding_vector = embedding_vector.to(device)
+    filtered_probes_tensor = filtered_probes_tensor.to(device)
+    
     # Compute cosine similarity
     cosine_sim = F.cosine_similarity(embedding_vector.unsqueeze(0), filtered_probes_tensor)
     
     # Get the closest value and index
     top_value, top_index = torch.topk(cosine_sim, 1)
     
+    # Create a range tensor on the same device
+    range_tensor = torch.arange(probes_tensor.size(0), device=device)
+
     # Convert the filtered tensor index to the original tensor index
-    original_index = torch.arange(probes_tensor.size(0))[non_zero_mask][top_index]
+    original_index = range_tensor[non_zero_mask][top_index]
     
     # Convert index to letter and build result list
     closest_probe = chr(original_index.item() + ord('a'))
     
     return closest_probe
+
+# This finds the farthest probe, or equivalently, the closest 'anti-probe'
+def find_closest_antiprobe(embedding_vector, probes_tensor):    
+    # probes_tensor should be shape-(26,4096), with rows corresponding to alphabet
+
+    # Check if all rows are zeros
+    if torch.all(probes_tensor == 0):
+        return '_'
+
+    # Create a mask to identify non-zero rows in probes_tensor
+    non_zero_mask = torch.any(probes_tensor != 0, dim=1)
+    
+    # Filter out zero rows from probes_tensor
+    filtered_probes_tensor = probes_tensor[non_zero_mask]
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    embedding_vector = embedding_vector.to(device)
+    filtered_probes_tensor = filtered_probes_tensor.to(device)  # Corrected line
+
+    # Compute cosine similarity with the negative of the embedding vector
+    cosine_sim = F.cosine_similarity((-1) * embedding_vector.unsqueeze(0), filtered_probes_tensor)
+    
+    # Get the closest value and index
+    top_value, top_index = torch.topk(cosine_sim, 1)
+
+    # Create a range tensor on the same device
+    range_tensor = torch.arange(probes_tensor.size(0), device=device)
+    
+    # Convert the filtered tensor index to the original tensor index
+    original_index = range_tensor[non_zero_mask][top_index]
+    
+    # Convert index to letter and build result list
+    closest_antiprobe = chr(original_index.item() + ord('a'))
+    
+    return closest_antiprobe
+
+
+# Returns a sorted dictionary of distances of the 26 probes encoded in weights_tensor (shape [26,4096]) to the [4096] tensor (embedding) emb
+def probe_distances(emb, weights_tensor):
+    # Determine the device (CUDA if available, otherwise CPU)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    # Move the embedding vector to the chosen device
+    emb = emb.to(device)
+
+    distance_dict = {}
+    for i, probe in enumerate(weights_tensor):
+        if probe.nelement() == 0:
+            continue  # Skip if probe is empty
+
+        # Move the probe to the same device as emb
+        probe = probe.to(device)
+
+        # Compute the cosine similarity
+        similarity = F.cosine_similarity(emb.unsqueeze(0), probe.unsqueeze(0))
+
+        # Convert similarity to distance and store it
+        distance = 1 - similarity.item()
+        distance_dict[chr(i + ord('A'))] = distance
+
+    # Sort by distance
+    sorted_distance_dict = {k: v for k, v in sorted(distance_dict.items(), key=lambda item: item[1])}
+
+    return sorted_distance_dict
+
+
+# prints dictionary of distances from embedding to all 26 probes, and plots a bar graph
+def display_closest_probes(emb, weights_tensor):
+
+    sorted_distance_dict = probe_distances(emb, weights_tensor)
+
+    print(sorted_distance_dict)
+
+    # Create a bar chart
+    plt.figure(figsize=(10, 6))
+    plt.bar(sorted_distance_dict.keys(), sorted_distance_dict.values())
+    plt.xlabel('FL probes')
+    plt.ylabel('cosine distance')
+    plt.title(f"Normalized Cosine Distances from embedding to all FL Probes")
+    plt.show()
+
+
 
 def predict_first_letter_with_probes(
         embeddings, starting_letter_probe_weights_tensor,
