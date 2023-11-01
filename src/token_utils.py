a2b0fded631b85a084beda985f8866c3e362e5b2
mwatkins1970 <108693675+mwatkins1970@users.noreply.github.com>
2023-11-01 16:49:55 +0000

    Update token_utils.py

--- src/token_utils.py
+++ src/token_utils.py
@@ -13,7 +13,6 @@ import numpy as np
 def token_setup(tokenizer):
     token_strings = [tokenizer.decode([i]) for i in range(50257)]
     num_tokens = len(token_strings)
-
     print(f"There are {num_tokens} tokens.")
 
     all_rom_tokens = []			#initialise list of all-roman tokens
@@ -28,10 +27,12 @@ def token_setup(tokenizer):
         all_rom_tokens.append(token_strings[i])
         all_rom_token_indices.append(i)
 
-    num_all_rom_tokens = len(all_rom_tokens)
-    print(f"There are {num_all_rom_tokens} all-Roman tokens.")
+    print(f"There are {len(all_rom_tokens)} all-Roman tokens.")
 
-    return token_strings, all_rom_token_indices
+    all_rom_token_gt2_indices = [idx for idx in all_rom_token_indices if len(token_strings[idx].strip()) > 2]
+    print(f"There are {len(all_rom_token_gt2_indices)} all-Roman tokens with more than two letters.")
+
+    return token_strings, all_rom_token_indices, all_rom_token_gt2_indices
 
 
 # THIS MAKES A LIST OF THE 50257 SIGNIFICANT TOKENS, AS WELL AS  SUB-LISTS OF ALL-ROMAN TOKENS
@@ -69,24 +70,43 @@ def load_token_strings_etc(tokenizer):
 		return token_strings, all_rom_tokens, all_rom_token_indices, all_rom_token_gt2_indices
 
 
-def closest_tokens(emb, top_k, token_strings, embeddings):
+def closest_tokens(emb, top_k, token_strings, embeddings, filter_CTC_num):
+    # 'filter_CTE_num is the number of "closest to centroid" tokens (which are also closest to everything else) you want to filter. 1000 works well.
+
+    if filter_CTC_num > 0:
+
+        print(f"FILTERING {filter_CTC_num} CTC TOKENS")
+
+        subset_embeddings = embeddings[:50257]
+        centroid = torch.mean(subset_embeddings, dim=0)
+        # Compute L2 distance from each vector to the centroid
+        # Euclidean distance can be calculated by taking the norm of the difference
+        distances = torch.norm(subset_embeddings - centroid, dim=1)
+        #Rank by distance (get the sorted indices)
+        sorted_indices = torch.argsort(distances)
+        # Convert the tensor to a Python list
+        sorted_indices_list = sorted_indices.tolist()[:filter_CTC_num]
+        set_of_CTC_token_indices = set(sorted_indices_list)
+    else:
+        set_of_CTC_token_indices = set()
+
+
 
     # Make sure all tensors are on the same device as `emb`
     embeddings = embeddings.to(emb.device)
     
     # Compute cosine similarity and subtract from 1 to get distance
     # Higher similarity means lower distance
-    #distances = 1 - F.cosine_similarity(embeddings[:50257], emb.unsqueeze(0).expand_as(embeddings[:50257]))
     distances = 1 - F.cosine_similarity(embeddings[:50257], emb)
 
 
     # Get the indices of the top k closest tokens
-    closest_indices = torch.argsort(distances)[:top_k]
-
-    #print(closest_indices)
+    closest_indices = torch.argsort(distances)
+    filtered_closest_indices = [idx.item() for idx in closest_indices if idx.item() not in set_of_CTC_token_indices]
+    closest_indices_list = filtered_closest_indices[:top_k]
     
     # Return the corresponding token strings for these indices
-    closest_tokens = [token_strings[i] for i in closest_indices]
+    closest_tokens = [token_strings[i] for i in closest_indices_list]
     
     return closest_tokens
 
@@ -120,7 +140,14 @@ def closest_tokens_recentred(emb, top_k, token_strings, embeddings):
 
 # Given a shape (26,4096) tensor (typically staring-letter probes), this
 # finds the closest k probes to the embedding vector and returns them 
-# as a zipped list, giving correspoding letters and cosine distances
+# as a zipped list, giving corresponding letters and cosine distances
+
+
+def euc_distance_from_centroid(emb, embeddings):
+    mean_emb = torch.mean(embeddings[:50257], dim=0)
+    euc_distance = torch.norm(emb - mean_emb, p=2)
+    return euc_distance
+
 
 def find_top_k_letters(embedding_vector, letter_tensor, k):
     # Compute cosine similarity
