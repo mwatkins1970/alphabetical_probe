b5341d7e236573ef5bbbb7a10ff8fd96b578cbbb
mwatkins1970 <108693675+mwatkins1970@users.noreply.github.com>
2023-11-10 22:50:46 +0000

    Update mutation_evals.py

--- src/mutation_evals.py
+++ src/mutation_evals.py
@@ -14,15 +14,8 @@ from find_closest_probe import find_closest_probe
 def mutation_evals_runner(results, upper_range, step, GPTmodel, tokenizer, embeddings, token_strings, all_rom_token_gt2_indices, num_shots):
 
     coeffs = [0, 1] + list(range(2, upper_range + 1, step))  # This merges the two lists into one
-
-    save_path = "/content/Drive/My Drive/SpellingMiracleCollab/first_letter_extended_results_checkpoints.pkl"  
-    
-    if os.path.exists(save_path):
-        # If a save file exists, load the progress from it.
-        with open(save_path, "rb") as f:
-            extended_results = pickle.load(f)
-    else:
-        extended_results = results  # Make a copy of results dictionary if no save file exists.
+ 
+    extended_results = results  # Make a copy of results dictionary if no save file exists.
 
     predictions = extended_results['predictions']  # We're going to enrich this and then replace the version in the copied dictionary.
 
@@ -36,15 +29,11 @@ def mutation_evals_runner(results, upper_range, step, GPTmodel, tokenizer, embed
             switch_flag = False
 
             for coeff in coeffs:
-                print(f"MUTATION COEFFICIENT: {coeff}")
+                print(f"MUTATION COEFFICIENT k: {coeff}")
                 _, results_mutated, switch_flag = mutant_first_letter_evals_runner(GPTmodel, tokenizer, embeddings, token_strings, all_rom_token_gt2_indices, index, num_shots, coeff, switch_flag)
                 # make the value for the coeff-key an ordered pair - prediction and logit)
                 prediction_dict['mutation predictions'][coeff] = (results_mutated['predictions'][0]['prompt prediction'], results_mutated['predictions'][0]['prediction logit'])
 
             print(f"{count + 1}/{len(predictions)}: Results dictionary now enriched to include first-letter predictions for mutations of embedding of token '{prediction_dict['token']}'\n")
-
-            # Save the progress after processing each token
-            with open(save_path, "wb") as f:
-                pickle.dump(extended_results, f)
 
     return extended_results
